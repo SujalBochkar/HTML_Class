04728b18eed5a35d898e2335418a8b7bf27e1f22
Finish55 <finishwhatyoustart55@gmail.com>
2022-11-03 23:20:43 +0530

    Bitwise

---
+++ Practice/21-10.py
@@ -0,0 +1,7 @@
+#bitwise
+x=int(input())
+y=int(input())
+if (x>0 and y>0) or (x<0 and y<0):
+    print("True")
+else :
+    print("False")
--- Practice/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-numb=int(input("enter the number to be reversed"))
-rev = 0
-while (numb > 0):
-    sum=0
-    remainder=numb % 10
-    rev=(rev * 10)+ remainder
-    numb = numb //10
-print(rev)
