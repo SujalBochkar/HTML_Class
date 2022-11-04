d74544c1c51fde518f363dd570b0ac5cef0c805e
Finish55 <finishwhatyoustart55@gmail.com>
2022-11-04 23:12:19 +0530

    21-10

--- Practice/21-10.py
+++ Practice/21-10.py
@@ -1,4 +1,4 @@
-# #bitwise
+# 1 bitwise
     # x=int(input())
     # y=int(input())
     # if (x>0 and y>0) or (x<0 and y<0):
@@ -6,16 +6,46 @@
     # else :
     #     print("False")
 
-# Area of trapezium
+# 2 Area of trapezium
     # a=float(input("A: "))
     # b=float(input("B: "))
     # h=float(input("H: "))
     # Area= 1/2*(a+b)*h
     # print("Area: {:.2f}".format(Area))
 
-#even 
-x=(input())
-if (int(x[0])%2) == 0 and (int(x[-1])%2) == 0:
-    print("True")
-else:
-    print("False")
+# 3 even 
+    # x=(input())
+    # if ((x[0])%2) == 0 and ((x[-1])%2) == 0:
+    #     print("True")
+    # else:
+    #     print("False")
+
+# 4 Same name 
+    # x = str(input())
+    # y = str(input())
+    # print(reversed(bool(x[0] == y[0])))
+
+# 5 contest 
+    # x=str(input())
+    # y=str(input())
+    # if ((x[-1] and x[-2]) in y ):
+    #     print("True")
+    # else :
+    #     print("False")
+
+# 6 large number
+    # x = input()
+    # y = int(x[0]) ** int(x[-1])
+    # print(y)
+    # if (int(y) % 2 == 0):
+    #     print("True")
+    # else:
+    #     print("False")
+
+
+
+
+
+
+
+
